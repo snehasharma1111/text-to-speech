6049b294a7d10e150ae62b946c6c37a2b0f168a2
Sneha Sharma <snehasharma9205@gmail.com>
2024-01-01 15:54:26 +0530

    created footer

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const text = form.querySelector('input').value;
-  const utterance = new SpeechSynthesisUtterance(text);
-  window.speechSynthesis.speak(utterance);
-})
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const text = form.querySelector("input").value;
+	const utterance = new SpeechSynthesisUtterance(text);
+	window.speechSynthesis.speak(utterance);
+});
+
+feather.replace();
